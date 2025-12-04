34e71f38370600351497b530826a6aef42c87196
Tim Kung <tim_kung@sfu.ca>
2025-12-03 17:05:34 -0800

    Update app.py

--- app.py
+++ app.py
@@ -58,19 +58,55 @@ def retrieve_with_pandas(query: str, top_k: int = 10):
     df["Similarity"] = df["Embedding"].apply(cosine_sim)
     return df.sort_values(by="Similarity", ascending=False).head(top_k)
 
+
+def clean_query_with_llm(query, history):
+    prompt_content = f"""    
+    Below is a history of the conversation so far, and a new question asked by the user that needs to be answered by searching in a knowledge base.
+    You have access to SFU IT Knowledge Base index with 100's of chunked documents.
+    Generate a search query based on the conversation and the new question.
+    Do not include cited source filenames and document names e.g info.txt or doc.pdf in the search query terms.
+    Do not include any text inside [] or <<>> in the search query terms.
+    Do not include any special characters like '+'.
+    If the question is not in English, translate the question to English before generating the search query.
+    If you cannot generate a search query, return just the number 0.
+
+    History:
+    {history}
+
+    Question:
+    {query}
+
+    Search Query:
+    """
+
+    response_stream = ollama.chat(
+        model='gemma3:4b', 
+        messages=[{'role': 'user', 'content': prompt_content}],
+        stream=True
+    )
+    return response_stream
+
+
 def generate_with_rag(query, history):
-    # 1. Retrieve
-    results = retrieve_with_pandas(query)
+    # Retrieve
+    search_query = clean_query_with_llm(query)
+    print(search_query)
+    results = retrieve_with_pandas(search_query)
+
     context_str = "\n\n---\n\n".join(results["Document"].tolist())
 
-    # 2. Construct Prompt
+    # Construct Prompt
     # We send this to Ollama, which is running efficiently in the background
       # Build a clean prompt
-    prompt_content = f"""You are an IT helpdesk assistant.
-If the user asked a question, answer the user's question with detailed step by step instructions: consider all the articles below.
-If the user asked a question and the answer is not in the contexts, say you don't know and suggest contacting SFU IT.
-If the user DID NOT ask a question, be friendly and ask how you can help them.
+    prompt_content = f"""
+    You are an SFU IT helpdesk chatbot.
+    Below is a history of the conversation so far, a new question asked by the user, and related article chunks to the user question.
+    If the user asked a question, answer the user's question with detailed step by step instructions: consider all the articles below.
+    If the user asked a question and the answer is not in the contexts, say you don't know and suggest contacting SFU IT.
+    If the user DID NOT ask a question, be friendly and ask how you can help them.
 
+History:
+{history}
 
 Question:
 {query}
