63926e23005268a22a14176216220e2b7a0f2dc0
Tim Kung <63133156+timothytzkung@users.noreply.github.com>
2025-12-03 21:13:09 -0800

    Fixed prompt engineering

--- app.py
+++ app.py
@@ -45,6 +45,9 @@ df = pd.DataFrame({
     "Embedding": list(embeddings),
 })
 
+# Chat history
+total_history = []
+
 # --- RAG Logic ---
 def retrieve_with_pandas(query: str, top_k: int = 10):
     query_embedding = embedding_model.encode([query])[0]
@@ -59,21 +62,14 @@ def retrieve_with_pandas(query: str, top_k: int = 10):
     return df.sort_values(by="Similarity", ascending=False).head(top_k)
 
 
-def clean_query_with_llm(query, history):
+def clean_query_with_llm(query, history): 
     prompt_content = f"""    
-    Below is a history of the conversation so far, and a new question asked by the user that needs to be answered by searching in a knowledge base.
+    Below is a new question asked by the user that needs to be answered by searching in a knowledge base.
     You have access to SFU IT Knowledge Base index with 100's of chunked documents.
-    Generate a search query based on the conversation and the new question.
-    Do not include cited source filenames and document names e.g info.txt or doc.pdf in the search query terms.
-    Do not include any text inside [] or <<>> in the search query terms.
-    Do not include any special characters like '+'.
-    If the question is not in English, translate the question to English before generating the search query.
+    Generate a search question based the user's question.
     If you cannot generate a search query, return just the number 0.
 
-    History:
-    {history}
-
-    Question:
+    User's Question:
     {query}
 
     Search Query:
@@ -82,14 +78,19 @@ def clean_query_with_llm(query, history):
     response_stream = ollama.chat(
         model='gemma3:4b', 
         messages=[{'role': 'user', 'content': prompt_content}],
-        stream=True
     )
-    return response_stream
+    response_string = response_stream['message']['content']
+    return response_string
 
 
 def generate_with_rag(query, history):
+
+    # goSFU specific cleaning
+    if "gosfu" in query.lower():
+        query = query.replace("gosfu", "goSFU") 
+
     # Retrieve
-    search_query = clean_query_with_llm(query)
+    search_query = clean_query_with_llm(query, history)
     print(search_query)
     results = retrieve_with_pandas(search_query)
 
@@ -99,15 +100,17 @@ def generate_with_rag(query, history):
     # We send this to Ollama, which is running efficiently in the background
       # Build a clean prompt
     prompt_content = f"""
-    You are an SFU IT helpdesk chatbot.
-    Below is a history of the conversation so far, a new question asked by the user, and related article chunks to the user question.
+    You are a SFU IT helpdesk chatbot.
+    Your task is to answer SFU IT related questions such as accessing various technology services or general troubleshooting. 
+    Below is new question asked by the user, and related article chunks to the user question.
     If the user asked a question, answer the user's question with detailed step by step instructions: consider all the articles below.
-    If the user asked a question and the answer is not in the contexts, say you don't know and suggest contacting SFU IT.
+    If there are links in the articles, provide those links in your answer.
+    If the user asked a question and the answer is not in the contexts, say that you're sorry that you can't help them and suggest contacting SFU IT at 778-782-8888 or by submitting an inquiry ticket at https://www.sfu.ca/information-systems/get-help.html
     If the user DID NOT ask a question, be friendly and ask how you can help them.
-
-History:
-{history}
-
+    Do not recommend, suggest, or provide any advice on anything that is not related to SFU or SFU IT.
+    If the user asked something relating to mental health or is seeking medical advice, redirect them to SFU Health & Counselling at https://www.sfu.ca/students/health.html
+    Do not ask the user any follow-up questions after answering them.
+   
 Question:
 {query}
 
